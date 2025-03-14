aa5c01ef10022b0938d1bcf2233d58926cdb8e70
salmaa26 <salmaabadruddinn26@gmail.com>
2025-03-14 16:20:12 +0700

    fix banget ini

--- src/components/atomics/footer.module.tsx
+++ src/components/atomics/footer.module.tsx
@@ -1,6 +1,4 @@
 export default function Footer() {
-  const currentYear = new Date().getFullYear();
-
   return (
     <footer className="bg-black text-bold text-white p-7 text-center w-full h-20 flex justify-center items-center">
       <div className="div">
