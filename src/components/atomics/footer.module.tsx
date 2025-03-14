e61d44cfaffe07f728134735e40bc320b9b97388
salmaa26 <salmaabadruddinn26@gmail.com>
2025-03-14 16:17:02 +0700

    fix image

--- src/components/atomics/footer.module.tsx
+++ src/components/atomics/footer.module.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="bg-black text-bold text-white p-7 text-center w-full h-20 flex justify-center items-center">
       <div className="div">
-      &copy; {currentYear} Enhancing Beauty, Empowering You!
+        <p> {currentYear} Enhancing Beauty Empowering You!</p>
       </div>
     </footer>
   );
